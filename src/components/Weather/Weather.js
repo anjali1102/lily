c9a89df8378659e853531e5e744cb15c3aa70e0c
anjali1102 <ac.chauhan1102@gmail.com>
2022-07-19 02:06:01 +0530

    [Fix]: improvements wrt reviews

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -3,12 +3,9 @@ import { WeatherBody } from "./WeatherBody";
 import "./WeatherBody.css";
 
 const Weather = () => {
-  const [lat, setLat] = useState([]);
-  const [long, setLong] = useState([]);
+  const [lat, setLat] = useState(0);
+  const [long, setLong] = useState(0);
   const [data, setData] = useState([]);
-
-  const REACT_APP_API_URL = "https://api.openweathermap.org/data/2.5";
-  const REACT_APP_API_KEY = "c962ed695546773fa54252b994d33923";
 
   useEffect(() => {
     const fetchData = async () => {
@@ -18,12 +15,11 @@ const Weather = () => {
       });
 
       await fetch(
-        `${REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${REACT_APP_API_KEY}`
+        `${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
       )
         .then((res) => res.json())
         .then((result) => {
           setData(result);
-          console.log(result);
         });
     };
     fetchData();
